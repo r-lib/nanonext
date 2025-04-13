8b0c87ffdc6c25b519214c51c974bc34d844e6f1
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-04-13 10:02:31 +0100

    Separate out source vignette and exclude from tarball

--- vignettes-src/precompile.R
+++ vignettes-src/precompile.R
@@ -1,2 +1,2 @@
 # Vignettes precompiled as require setup of communications between separate processes, python etc.
-knitr::knit("vignettes/nanonext.Rmd.orig", "vignettes/nanonext.Rmd")
+knitr::knit("vignettes-src/_nanonext.Rmd", "vignettes/nanonext.Rmd")
