2efde96a18f7660442a4e365dff707f3e77c058b
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-04-25 11:10:49 +0100

    Additional safety for zero-length R vectors

--- src/tls.c
+++ src/tls.c
@@ -101,7 +101,8 @@ SEXP rnng_random(SEXP n, SEXP convert) {
     out = nano_hash_char(buf, sz);
   } else {
     out = Rf_allocVector(RAWSXP, sz);
-    memcpy(NANO_DATAPTR(out), buf, sz);
+    if (sz)
+      memcpy(NANO_DATAPTR(out), buf, sz);
   }
 
   return out;
