r-lib/nanonext

commit debdfa5ef58980c3860af55a0b43dc311b611dc6

Tidy ups

src/aio.c
@@ -20,6 +20,21 @@ static SEXP mk_error_aio(const int xc, SEXP env) {
 
 // aio completion callbacks ----------------------------------------------------
 
+static inline void nano_list_free(nano_node *list) {
+
+  while (list != NULL) {
+    nano_node *current = list;
+    list = list->next;
+    nano_aio *saio = (nano_aio *) current->data;
+    nng_aio_free(saio->aio);
+    if (saio->data != NULL)
+      free(saio->data);
+    free(saio);
+    free(current);
+  }
+
+}
+
 void nano_list_do(nano_list_op listop, nano_aio *saio) {
 
   static nano_node *free_list = NULL;
@@ -32,7 +47,7 @@ void nano_list_do(nano_list_op listop, nano_aio *saio) {
     break;
   case FINALIZE:
     nng_mtx_lock(free_mtx);
-    nano_list_do(FREE, NULL);
+    nano_list_free(free_list);
     if (saio->mode == 0x1) {
       nng_mtx_unlock(free_mtx);
       nng_aio_free(saio->aio);
@@ -60,24 +75,13 @@ void nano_list_do(nano_list_op listop, nano_aio *saio) {
   case SHUTDOWN:
     if (free_mtx == NULL) break;
     nng_mtx_lock(free_mtx);
-    nano_list_do(FREE, NULL);
+    nano_list_free(free_list);
     nng_mtx_unlock(free_mtx);
     nng_mtx_free(free_mtx);
     free_mtx = NULL;
     break;
-  case FREE: // must be entered under lock
-    while (free_list != NULL) {
-      nano_node *current = free_list;
-      free_list = free_list->next;
-      nano_aio *data = (nano_aio *) current->data;
-      nng_aio_free(data->aio);
-      if (data->data != NULL)
-        free(data->data);
-      free(data);
-      free(current);
-    }
-    break;
   }
+
 }
 
 static void saio_complete(void *arg) {

src/nanonext.h
@@ -235,7 +235,6 @@ typedef enum nano_list_op {
   INIT,
   FINALIZE,
   COMPLETE,
-  FREE,
   SHUTDOWN
 } nano_list_op;
 

src/sync.c
@@ -39,7 +39,7 @@ static void request_complete(void *arg) {
   } else if (res == 5) {
     const int id = saio->msgid;
     if (id) {
-      nng_msg *msg;
+      nng_msg *msg = NULL;
       if (nng_msg_alloc(&msg, 0) == 0) {
         if (nng_msg_append_u32(msg, 0) ||
             nng_msg_append(msg, &id, sizeof(id)) ||
@@ -473,7 +473,7 @@ SEXP rnng_request(SEXP con, SEXP data, SEXP sendmode, SEXP recvmode, SEXP timeou
 
   PROTECT(aio = R_MakeExternalPtr(raio, nano_AioSymbol, NANO_PROT(con)));
   R_RegisterCFinalizerEx(aio, request_finalizer, TRUE);
-  Rf_setAttrib(aio, nano_SocketSymbol, con);
+  Rf_setAttrib(aio, nano_ContextSymbol, con);
   Rf_setAttrib(aio, nano_MsgidSymbol, Rf_ScalarInteger(id));
 
   PROTECT(env = R_NewEnv(R_NilValue, 0, 0));

commit 927ac5e6f0b144ea156449d0fb66e59eb9e94a33

Fix subtle bug introduced by debdfa5

DESCRIPTION
@@ -1,7 +1,7 @@
 Type: Package
 Package: nanonext
 Title: NNG (Nanomsg Next Gen) Lightweight Messaging Library
-Version: 1.5.2.9020
+Version: 1.5.2.9021
 Authors@R: c(
     person("Charlie", "Gao", , "charlie.gao@posit.co", role = c("aut", "cre"),
            comment = c(ORCID = "0000-0002-0750-061X")),

src/aio.c
@@ -20,21 +20,6 @@ static SEXP mk_error_aio(const int xc, SEXP env) {
 
 // aio completion callbacks ----------------------------------------------------
 
-static inline void nano_list_free(nano_node *list) {
-
-  while (list != NULL) {
-    nano_node *current = list;
-    list = list->next;
-    nano_aio *saio = (nano_aio *) current->data;
-    nng_aio_free(saio->aio);
-    if (saio->data != NULL)
-      free(saio->data);
-    free(saio);
-    free(current);
-  }
-
-}
-
 void nano_list_do(nano_list_op listop, nano_aio *saio) {
 
   static nano_node *free_list = NULL;
@@ -47,7 +32,7 @@ void nano_list_do(nano_list_op listop, nano_aio *saio) {
     break;
   case FINALIZE:
     nng_mtx_lock(free_mtx);
-    nano_list_free(free_list);
+    nano_list_do(FREE, NULL);
     if (saio->mode == 0x1) {
       nng_mtx_unlock(free_mtx);
       nng_aio_free(saio->aio);
@@ -75,10 +60,22 @@ void nano_list_do(nano_list_op listop, nano_aio *saio) {
   case SHUTDOWN:
     if (free_mtx == NULL) break;
     nng_mtx_lock(free_mtx);
-    nano_list_free(free_list);
+    nano_list_do(FREE, NULL);
     nng_mtx_unlock(free_mtx);
     nng_mtx_free(free_mtx);
     free_mtx = NULL;
+    break;
+  case FREE: // must be entered under lock
+    while (free_list != NULL) {
+      nano_node *current = free_list;
+      free_list = free_list->next;
+      nano_aio *saio = (nano_aio *) current->data;
+      nng_aio_free(saio->aio);
+      if (saio->data != NULL)
+        free(saio->data);
+      free(saio);
+      free(current);
+    }
     break;
   }
 

src/nanonext.h
@@ -235,6 +235,7 @@ typedef enum nano_list_op {
   INIT,
   FINALIZE,
   COMPLETE,
+  FREE,
   SHUTDOWN
 } nano_list_op;
 