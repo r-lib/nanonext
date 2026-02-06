471450f7ec228375ca5180168367f9effb106272
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2026-02-06 00:02:04 +0000

    Add web utilities vignette

--- dev/vignettes/precompile.R
+++ dev/vignettes/precompile.R
@@ -1,2 +1,3 @@
 # Vignettes precompiled as require setup of communications between separate processes, python etc.
 knitr::knit("dev/vignettes/_nanonext.Rmd", "vignettes/nanonext.Rmd")
+knitr::knit("dev/vignettes/_web.Rmd", "vignettes/web.Rmd")
