1eb07375963537cb16372db3fd3488c8c4cbd035
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2026-02-06 12:22:48 +0000

    Create new vignettes (#245)
    * Create new vignettes
    
    * Update news with recent changes
    
    * Refresh readme

--- dev/vignettes/precompile.R
+++ dev/vignettes/precompile.R
@@ -1,3 +1,5 @@
 # Vignettes precompiled as require setup of communications between separate processes, python etc.
-knitr::knit("dev/vignettes/_nanonext.Rmd", "vignettes/nanonext.Rmd")
-knitr::knit("dev/vignettes/_web.Rmd", "vignettes/web.Rmd")
+knitr::knit("dev/vignettes/_v01-messaging.Rmd", "vignettes/v01-messaging.Rmd")
+knitr::knit("dev/vignettes/_v02-protocols.Rmd", "vignettes/v02-protocols.Rmd")
+knitr::knit("dev/vignettes/_v03-configuration.Rmd", "vignettes/v03-configuration.Rmd")
+knitr::knit("dev/vignettes/_v04-web.Rmd", "vignettes/v04-web.Rmd")
