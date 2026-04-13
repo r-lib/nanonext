7e623ceba781c9ef718dbb9870b574e2cddcddc6
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2026-04-13 20:10:06 +0100

    fix: avoid potential size_t overflow in iov clamping check

--- src/nng/src/platform/posix/posix_tcpconn.c
+++ src/nng/src/platform/posix/posix_tcpconn.c
@@ -61,7 +61,7 @@ tcp_dowrite(nni_tcp_conn *c)
 		for (niov = 0, i = 0; i < naiov; i++) {
 			if (aiov[i].iov_len > 0) {
 				size_t len = aiov[i].iov_len;
-				if (count + len > INT_MAX)
+				if (len > INT_MAX - count)
 					len = INT_MAX - count;
 				iovec[niov].iov_len  = len;
 				iovec[niov].iov_base = aiov[i].iov_buf;
@@ -128,7 +128,7 @@ tcp_doread(nni_tcp_conn *c)
 		for (niov = 0, i = 0; i < naiov; i++) {
 			if (aiov[i].iov_len != 0) {
 				size_t len = aiov[i].iov_len;
-				if (count + len > INT_MAX)
+				if (len > INT_MAX - count)
 					len = INT_MAX - count;
 				iovec[niov].iov_len  = len;
 				iovec[niov].iov_base = aiov[i].iov_buf;
